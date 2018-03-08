10ea1951936ab56e5ec05daf6f21ebee29ad089e
EdwardMudaida <eddieatsenga@protonmail.com>
2018-03-08 16:00:42 +0300

    Render Home component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
+import Home from '../Home/Home';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Home/>
             </div>
         );
     }
