a19431df4126a5295ced6abdb531b8ebf29e9c90
EdwardMudaida <eddieatsenga@protonmail.com>
2018-03-12 14:04:45 +0300

    Add  index and events routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
+import ShowEvents from '../ShowEvents/ShowEvents';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header/>
-                <Home/>
-            </div>
+            <Router>
+                <div>
+                    <Header/>
+                    <Route path='/' component={Home} exact />
+                    <Route path='/events' component={ShowEvents} />
+                </div>
+            </Router>
         );
     }
 }
