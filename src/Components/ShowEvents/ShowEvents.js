06394db6a15f902dcebaa7390242875ad56e3597
EdwardMudaida <eddieatsenga@protonmail.com>
2018-03-28 17:22:10 +0300

    [ Fix ] Add SowEvents to list events

---
+++ src/Components/ShowEvents/ShowEvents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ShowEvents() {
+    document.title = 'Bright Events | Events';
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default ShowEvents;
