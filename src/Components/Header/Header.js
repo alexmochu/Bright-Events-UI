a99c720fbbcd0e67541c6bb01c5c597232fb1729
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-04 16:41:05 +0300

    Add Login link to the nav-bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,51 +7,52 @@ class Header extends Component {
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
-    const { activeItem } = this.state
+      const { activeItem } = this.state;
 
-    return (
-      <Menu>
-        <Menu.Item
-          name='home'
-          position='left'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-          href='/'
-        >
-          <img src="http://pngimg.com/uploads/letter_b/letter_b_PNG13.png" alt="Home"/>
-        </Menu.Item>
+      return (
+          <Menu>
+              <Menu.Item
+                  name='home'
+                  position='left'
+                  active={activeItem === 'home'}
+                  onClick={this.handleItemClick}
+                  href='/'
+              >
+                  <img src="http://pngimg.com/uploads/letter_b/letter_b_PNG13.png" alt="Home"/>
+              </Menu.Item>
 
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='browse-events'
-            color='orange'
-            active={activeItem === 'browse-events'}
-            onClick={this.handleItemClick}
-            href='/events'
-          >
+              <Menu.Menu position='right'>
+                  <Menu.Item
+                      name='browse-events'
+                      color='orange'
+                      active={activeItem === 'browse-events'}
+                      onClick={this.handleItemClick}
+                      href='/events'
+                  >
             Browse Events
-          </Menu.Item>
+                  </Menu.Item>
 
-          <Menu.Item
-            name='create-events'
-            color='orange'
-            active={activeItem === 'create-events'}
-            onClick={this.handleItemClick}
-          >
+                  <Menu.Item
+                      name='create-events'
+                      color='orange'
+                      active={activeItem === 'create-events'}
+                      onClick={this.handleItemClick}
+                  >
             Create Events
-          </Menu.Item>
+                  </Menu.Item>
 
-          <Menu.Item
-            name='sing-in'
-            color='orange'
-            active={activeItem === 'sing-in'}
-            onClick={this.handleItemClick}
-          >
-            Sign In
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-    )
+                  <Menu.Item
+                      name='login'
+                      color='orange'
+                      active={activeItem === 'login'}
+                      onClick={this.handleItemClick}
+                      href='/login'
+                  >
+            Login
+                  </Menu.Item>
+              </Menu.Menu>
+          </Menu>
+      );
   }
 }
 
