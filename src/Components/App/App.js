fd94ae588f8d4dcb3eaf7661ea38a8a96c531154
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-04 16:39:19 +0300

    Add route to Login component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import ShowEvents from '../ShowEvents/ShowEvents';
+import Login from '../Login/Login';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
                     <Header/>
                     <Route path='/' component={Home} exact />
                     <Route path='/events' component={ShowEvents} />
+                    <Route path='/login' component={Login} />
                 </div>
             </Router>
         );
