51abd99fd72ba63988660c65f15d7c494e9b1ef2
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-12 23:09:41 +0300

    [Feature #156501370] Store JWT

--- src/index.js
+++ src/index.js
@@ -10,12 +10,18 @@ import './index.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
+import { userLoggedIn } from './actions/auth';
 
 
 const store = createStore(
     rootReducer, 
     composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.JWT) {
+    const user = { auth_token: localStorage.JWT };
+    store.dispatch(userLoggedIn(user));
+};
 
 ReactDOM.render(
     <Provider store={store}>
