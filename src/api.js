ae3bc82b47d4608a78914dc6bc99413aea859e9c
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-17 23:34:18 +0300

    [ Fix ] Remove /api/v1 prefix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import client from './client';
 
 export default {
     user: {
-        login: (credentials) => client.post('/api/v1/auth/login', { credentials }).then(res => res.data.user),
-        signup: user => client.post('/api/v1/auth/register', { user }).then(res => res.data.user),
+        login: (credentials) => client.post('/auth/login', { credentials }).then(res => res.data.user),
+        signup: user => client.post('/auth/register', { user }).then(res => res.data.user),
     }
 };
