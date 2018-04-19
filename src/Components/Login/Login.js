08e02a7ca069dad9afc26277967c216e0f9812f1
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-19 15:31:56 +0300

    [Feature #156501466] Redirect to /event/new after login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,7 +9,7 @@ import LoginForm from '../forms/LoginForm';
 
 class Login extends React.Component {
     submit = (data) =>
-        this.props.login(data).then(() => this.props.history.push('/'));
+        this.props.login(data).then(() => this.props.history.push('/event/new'));
     
     render() {
         document.title = 'Bright Events | Login';
