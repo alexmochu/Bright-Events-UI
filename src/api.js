90515c028b4962eec2f46f33cb137fd38fa438f3
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-19 15:19:17 +0300

    [Feature #156501466] Make API call to POST /events

--- src/api.js
+++ src/api.js
@@ -2,7 +2,10 @@ import client from './client';
 
 export default {
     user: {
-        login: (credentials) => client.post('/auth/login', { credentials }).then(res => res.data.user),
+        login: credentials => client.post('/auth/login', { credentials }).then(res => res.data.user),
         signup: user => client.post('/auth/register', { user }).then(res => res.data.user),
+    },
+    event: {
+        create: event => client.post('/events', { event }).then(res => res.data.event)
     }
 };
