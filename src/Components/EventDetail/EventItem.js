8372b324e0995eed2d4cfd0e67b5ee2fbf28d0b7
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-19 15:32:39 +0300

    [Feature #156501466] Source loader externally

--- src/Components/EventDetail/EventItem.js
+++ src/Components/EventDetail/EventItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Label, Message, Icon, Segment, Button } from 'semantic-ui-react';
+import { Container, Label, Icon, Segment, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-
+import Loading from '../loading/Loading';
 import client from '../../client';
 import './EventItem.css';
 
@@ -27,51 +27,42 @@ class EventItem extends Component{
 
     render(){
         const  event  = this.state.eventDetails.event;
-        if (this.state.eventDetails.event){
-            return(
-                <div>
-                    <Container>
-                        <div>
-                            <br/>
-                            <Segment.Group raised>
-                                <Segment className='event-title'><h1>{event.title}</h1></Segment>
-                                <Segment.Group>
-                                    <Segment><p className='event-description'>{event.description}</p></Segment>
-                                </Segment.Group>
-                                <Segment.Group horizontal>
-                                    <Segment><h4 className='event-details'>When: {event.date}</h4></Segment>
-                                    <Segment><h4 className='event-details'>Where: {event.location}</h4></Segment>
-                                    <Segment>
-                                        <Button animated positive>
-                                            <Button.Content visible>RSVP</Button.Content>
-                                            <Button.Content hidden>
-                                                <Icon name='right arrow' />
-                                            </Button.Content>
-                                        </Button>
-                                        <Button icon='edit' content='Edit' />
-                                        <Button icon='delete' negative content='Delete' />
-                                    </Segment>
-                                </Segment.Group>
-                                <Segment><Label horizontal><span className='event-category'>#{event.category}</span></Label></Segment>
-                            </Segment.Group>
-                        </div>
-                    </Container>
-                </div>
-            );
-        } else {
-            return (
+        if (!this.state.eventDetails.event){
+            return <Loading/>;
+        }
+        return(
+            <div>
                 <Container>
-                    <br/>
-                    <Message icon>
-                        <Icon name='circle notched' loading />
-                        <Message.Content>
-                            <Message.Header>Just one second</Message.Header>
-                            We are fetching that content for you.
-                        </Message.Content>
-                    </Message>
+                    <div>
+                        <br/>
+                        <Segment.Group raised>
+                            <Segment className='event-title'><h1>{event.title}</h1></Segment>
+                            <Segment.Group>
+                                <Segment><p className='event-description'>{event.description}</p></Segment>
+                            </Segment.Group>
+                            <Segment.Group horizontal>
+                                <Segment>
+                                    <h4 className='event-details'>When: {event.date}</h4>
+                                    <h4 className='event-details'>Time: {event.time}</h4>
+                                </Segment>
+                                <Segment><h4 className='event-details'>Where: {event.location}</h4></Segment>
+                                <Segment>
+                                    <Button animated positive>
+                                        <Button.Content visible>RSVP</Button.Content>
+                                        <Button.Content hidden>
+                                            <Icon name='right arrow' />
+                                        </Button.Content>
+                                    </Button>
+                                    <Button icon='edit' content='Edit' />
+                                    <Button icon='delete' negative content='Delete' />
+                                </Segment>
+                            </Segment.Group>
+                            <Segment><Label horizontal><span className='event-category'>#{event.category}</span></Label></Segment>
+                        </Segment.Group>
+                    </div>
                 </Container>
-            );
-        } 
+            </div>
+        );
     }
 }
 
