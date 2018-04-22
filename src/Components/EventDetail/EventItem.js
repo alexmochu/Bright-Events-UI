d196aef1cf237b5ee71c73ea3df619d1384b848f
EdwardMudaida <eddieatsenga@protonmail.com>
2018-04-22 22:45:23 +0300

    [Feature #156501481] RSVP event

--- src/Components/EventDetail/EventItem.js
+++ src/Components/EventDetail/EventItem.js
@@ -33,6 +33,14 @@ class EventItem extends Component{
             );
     }
 
+    rsvpEvent() {
+        let eventId = this.props.match.params.id;
+        client.post(`/events/${eventId}/rsvp`)
+            .then(
+                () => this.props.history.push(`/events/${eventId}/`)
+            );
+    }
+
     render(){
         const  event  = this.state.eventDetails.event;
         if (!this.state.eventDetails.event){
@@ -55,13 +63,8 @@ class EventItem extends Component{
                                 </Segment>
                                 <Segment><h4 className='event-details'>Where: {event.location}</h4></Segment>
                                 <Segment>
-                                    <Button animated positive>
-                                        <Button.Content visible>RSVP</Button.Content>
-                                        <Button.Content hidden>
-                                            <Icon name='right arrow' />
-                                        </Button.Content>
-                                    </Button>
-                                    <Button icon='edit' content='Edit' />
+                                    <Button compact color='green' size='large' onClick={() => this.rsvpEvent()}>RSVP</Button>
+                                    <Button icon='edit' content='Edit'/>
                                     <Button icon='delete' negative content='Delete' onClick={() => this.deleteEvent()}/>
                                 </Segment>
                             </Segment.Group>
