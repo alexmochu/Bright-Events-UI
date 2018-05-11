5abe3851f5c00a1c668ad5cadc2db1baec6893f9
EdwardMudaida <eddieatsenga@protonmail.com>
2018-05-11 08:45:57 +0300

    [Feature #157117367] Check if current_user has rsvp'd to an event

--- src/Components/EventDetail/EventItem.js
+++ src/Components/EventDetail/EventItem.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Container, Label, Segment, Button, Table } from 'semantic-ui-react';
+import { Container, Label, Button, Image, List  } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Loading from '../loading/Loading';
 import client from '../../client';
 import './EventItem.css';
+import userImage from '../../default-user.png';
 
 class EventItem extends Component{
     constructor(props){
@@ -13,8 +15,17 @@ class EventItem extends Component{
             eventDetails: {},
             rsvpbtnContent: 'RSVP',
             removersvpbtnContent: 'REMOVE RSVP',
-            guests: []
+            guests: [],
+            rsvpd: {}
         };
+    }
+  
+    componentDidMount() {
+        let eventId = this.props.match.params.id;
+        client.get(`/events/${eventId}/user/rsvp`)
+            .then(res => {
+                this.setState({ rsvpd: res.data });
+            });
     }
 
     componentWillMount = () => {
@@ -41,8 +52,11 @@ class EventItem extends Component{
     rsvpEvent() {
         let eventId = this.props.match.params.id;
         client.post(`/events/${eventId}/rsvp`)
-            .then(
-                this.setState({ rsvpbtnContent: 'Reserved'})
+            .then(res => {
+                this.setState({ 'rsvpd': res.data });
+                this.setState({ rsvpbtnContent: 'Reserved' }),
+                console.log('STATE', this.state);
+            },
             );
     }
 
@@ -50,7 +64,7 @@ class EventItem extends Component{
         let eventId = this.props.match.params.id;
         client.delete(`/events/${eventId}/rsvp`)
             .then(
-                this.setState({ removersvpbtnContent: 'RSVP REMOVED'})
+                this.setState({ removersvpbtnContent: 'RSVP REMOVED' })
             );
     }
 
@@ -65,53 +79,51 @@ class EventItem extends Component{
     render(){
         const event = this.state.eventDetails.event;
         const guests = this.state.guests;
+        const { currentUserId } = this.props;
         if (!this.state.eventDetails.event){
             return <Loading/>;
         }
         return(
             <div>
-                <Container style={{ marginTop: '7em' }}>
-                    <div>
+                <Container style={{ marginTop: '4em' }}>
+                    <div className='event-details'>
                         <br/>
-                        <Segment.Group raised>
-                            <Segment className='event-title'><h1>{event.title}</h1></Segment>
-                            <Segment.Group>
-                                <Segment><p className='event-description'>{event.description}</p></Segment>
-                            </Segment.Group>
-                            <Segment.Group horizontal>
-                                <Segment>
-                                    <h4 className='event-details'>When: {event.date}</h4>
-                                    <h4 className='event-details'>Time: {event.time}</h4>
-                                </Segment>
-                                <Segment><h4 className='event-details'>Where: {event.location}</h4></Segment>
-                                <Segment>
-                                    <Button compact positive basic size='large' content={ this.state.rsvpbtnContent } onClick={() => this.rsvpEvent()}/>
-                                    <Button icon='edit' color='black' basic content='EDIT'/>
-                                    <Button icon='delete' basic negative content='DELETE' onClick={() => this.deleteEvent()}/>
-                                    <Button negative basic content={ this.state.removersvpbtnContent }  onClick={() => this.removeRsvp()}/>
-                                </Segment>
-                            </Segment.Group>
-                            <Segment><Label horizontal><span className='event-category'>#{event.category}</span></Label></Segment>
-                        </Segment.Group>
+                        <h3>{event.date}</h3>
+                        <h1>{event.title}</h1>
+                        <p>{event.description}</p>
+                        <p>{event.location}</p> 
+                        <p>{event.time}</p>
+                        { this.state.rsvpd.message === 'True'?
+                            <Button color='orange'  content={ this.state.removersvpbtnContent } onClick={() => this.removeRsvp()}/>
+                            :
+                            <Button positive size='medium' content={ this.state.rsvpbtnContent } onClick={() => this.rsvpEvent()}/>
+                        }
+
+                        { currentUserId === event.user_id?
+                            <span>
+                                <Button icon='edit' content='EDIT'/>
+                                <Button icon='delete' negative content='DELETE' onClick={() => this.deleteEvent()}/>
+                            </span>
+                            :
+                            ''
+                        }
+                        <div className='event-category'>
+                            <Label basic color='black' size='small'><h5>#{event.category}</h5></Label>
+                        </div>
                     </div>
-                    <Container className='guests-container'>
-                        <h2 className='center guests-h1'>GUESTS</h2>
-                        <Table color='orange'>
-                            <Table.Header>
-                                <Table.Row>
-                                    <Table.HeaderCell>Username</Table.HeaderCell>
-                                    <Table.HeaderCell>Email</Table.HeaderCell>
-                                </Table.Row>
-                            </Table.Header>
+                    <Container className='guests-container' style={{ marginTop: '4em' }} floated='left'>
+                        <h3>GUESTS</h3>
+                        <List horizontal ordered>
                             {guests.map(guest =>
-                                <Table.Body key={guest.id}>
-                                    <Table.Row>
-                                        <Table.Cell>{guest.user_name}</Table.Cell>
-                                        <Table.Cell>{guest.email}</Table.Cell>
-                                    </Table.Row>
-                                </Table.Body>
+                                <List.Item key={guest.id}>
+                                    <Image avatar src={userImage} />
+                                    <List.Content>
+                                        <List.Header>{guest.user_name}</List.Header>
+                                        {guest.email}
+                                    </List.Content>
+                                </List.Item>
                             )}
-                        </Table>
+                        </List>
                     </Container>
                 </Container>
             </div>
@@ -123,7 +135,12 @@ EventItem.propTypes = {
     match: PropTypes.object.isRequired,
     history: PropTypes.shape({
         push: PropTypes.func.isRequired
-    }).isRequired
+    }).isRequired,
+    currentUserId: PropTypes.number.isRequired
 };
 
-export default EventItem;
+const mapStateToProps = state =>({
+    currentUserId: state.auth.user.sub
+});
+
+export default connect(mapStateToProps)(EventItem);
